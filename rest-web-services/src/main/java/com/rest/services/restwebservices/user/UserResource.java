910acd4414e8d64c78cb03300498b3c6cd789742
Arvind <europa1613@users.noreply.github.com>
2018-02-11 17:50:12 -0600

    returning ResponseEntity with created status and location

--- rest-web-services/src/main/java/com/rest/services/restwebservices/user/UserResource.java
+++ rest-web-services/src/main/java/com/rest/services/restwebservices/user/UserResource.java
@@ -1,13 +1,16 @@
 package com.rest.services.restwebservices.user;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 @RestController
 public class UserResource {
@@ -26,7 +29,11 @@ public class UserResource {
 	}
 
 	@PostMapping("/users")
-	public User createUser(@RequestBody User user) {
-		return service.save(user);
+	public ResponseEntity<Object> createUser(@RequestBody User user) {
+		User savedUser = service.save(user);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(savedUser.getId())
+				.toUri();
+
+		return ResponseEntity.created(uri).build();
 	}
 }
