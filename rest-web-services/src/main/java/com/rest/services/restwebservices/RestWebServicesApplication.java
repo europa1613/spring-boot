746572fbed7c940084836fbcaf2064aeb8abf223
Arvind <europa1613@users.noreply.github.com>
2018-02-11 15:43:13 -0600

    Initial commit - Spring Initialzr generated

---
+++ rest-web-services/src/main/java/com/rest/services/restwebservices/RestWebServicesApplication.java
@@ -0,0 +1,12 @@
+package com.rest.services.restwebservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestWebServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestWebServicesApplication.class, args);
+	}
+}
