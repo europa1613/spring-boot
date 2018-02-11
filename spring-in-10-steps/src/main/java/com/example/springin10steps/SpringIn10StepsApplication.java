23f592aaf5c0d1cdd003fcb4b8ec07cdfd8f303f
Arvind <europa1613@users.noreply.github.com>
2018-02-10 18:07:50 -0600

    Spring boot in 10 steps

---
+++ spring-in-10-steps/src/main/java/com/example/springin10steps/SpringIn10StepsApplication.java
@@ -0,0 +1,12 @@
+package com.example.springin10steps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringIn10StepsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringIn10StepsApplication.class, args);
+	}
+}
