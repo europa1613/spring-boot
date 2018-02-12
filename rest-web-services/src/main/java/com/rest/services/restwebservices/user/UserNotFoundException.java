c28b59e1abe5308dd0ba64456476f439187bdc19
Arvind <europa1613@users.noreply.github.com>
2018-02-11 18:14:22 -0600

    Add ResponseStatus -- Not found on exception

--- rest-web-services/src/main/java/com/rest/services/restwebservices/user/UserNotFoundException.java
+++ rest-web-services/src/main/java/com/rest/services/restwebservices/user/UserNotFoundException.java
@@ -1,5 +1,9 @@
 package com.rest.services.restwebservices.user;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.NOT_FOUND)
 public class UserNotFoundException extends RuntimeException {
 
 	/**
