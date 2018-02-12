3e4b1888e9d7104b7d46ca9fd8f37329b143500c
Arvind <europa1613@users.noreply.github.com>
2018-02-11 21:18:15 -0600

    javaz.validation - validate request

--- rest-web-services/src/main/java/com/rest/services/restwebservices/user/User.java
+++ rest-web-services/src/main/java/com/rest/services/restwebservices/user/User.java
@@ -2,12 +2,17 @@ package com.rest.services.restwebservices.user;
 
 import java.util.Date;
 
+import javax.validation.constraints.Past;
+import javax.validation.constraints.Size;
+
 public class User {
 
 	private Integer id;
 
+	@Size(min=2)
 	private String name;
 
+	@Past
 	private Date birthDate;
 
 	public User(Integer id, String name, Date birthDate) {
--- rest-web-services/src/main/java/com/rest/services/restwebservices/user/UserResource.java
+++ rest-web-services/src/main/java/com/rest/services/restwebservices/user/UserResource.java
@@ -3,6 +3,8 @@ package com.rest.services.restwebservices.user;
 import java.net.URI;
 import java.util.List;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -32,7 +34,7 @@ public class UserResource {
 		}
 		return user;
 	}
-	
+
 	@DeleteMapping("/users/{id}")
 	public User removeUser(@PathVariable int id) {
 		User user = service.deleteUserById(id);
@@ -43,7 +45,7 @@ public class UserResource {
 	}
 
 	@PostMapping("/users")
-	public ResponseEntity<Object> createUser(@RequestBody User user) {
+	public ResponseEntity<Object> createUser(@Valid @RequestBody User user) {
 		User savedUser = service.save(user);
 		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(savedUser.getId())
 				.toUri();
