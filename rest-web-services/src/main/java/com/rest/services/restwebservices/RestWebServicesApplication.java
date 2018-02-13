19235b028c9381ab0c8cc62d0149b70a362bdb40
Arvind <europa1613@users.noreply.github.com>
2018-02-12 22:23:04 -0600

    Internationalization

--- rest-web-services/src/main/java/com/rest/services/restwebservices/RestWebServicesApplication.java
+++ rest-web-services/src/main/java/com/rest/services/restwebservices/RestWebServicesApplication.java
@@ -1,12 +1,32 @@
 package com.rest.services.restwebservices;
 
+import java.util.Locale;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 
 @SpringBootApplication
 public class RestWebServicesApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(RestWebServicesApplication.class, args);
+	}
+
+	@Bean
+	public LocaleResolver localeResolver() {
+		SessionLocaleResolver localeResolver = new SessionLocaleResolver();
+		localeResolver.setDefaultLocale(Locale.US);
+		return localeResolver;
+	}
+
+	@Bean(name="messageSource")
+	public ResourceBundleMessageSource bundleSource() {
+		ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+		messageSource.setBasename("messages");
+		return messageSource;
 	}
 }
--- rest-web-services/src/main/java/com/rest/services/restwebservices/user/UserResource.java
+++ rest-web-services/src/main/java/com/rest/services/restwebservices/user/UserResource.java
@@ -2,10 +2,12 @@ package com.rest.services.restwebservices.user;
 
 import java.net.URI;
 import java.util.List;
+import java.util.Locale;
 
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
 import org.springframework.hateoas.Resource;
 import org.springframework.hateoas.mvc.ControllerLinkBuilder;
 import org.springframework.http.ResponseEntity;
@@ -14,11 +16,15 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 @RestController
 public class UserResource {
+
+	@Autowired
+	private MessageSource messageSource;
 
 	@Autowired
 	private UserDAOService service;
@@ -39,7 +45,7 @@ public class UserResource {
 
 		ControllerLinkBuilder linkToUsers = ControllerLinkBuilder
 				.linkTo(ControllerLinkBuilder.methodOn(this.getClass()).retrieveAllUsers());
-		
+
 		resource.add(linkToUsers.withRel("all-users"));
 
 		return resource;
@@ -61,5 +67,10 @@ public class UserResource {
 				.toUri();
 
 		return ResponseEntity.created(uri).build();
+	}
+
+	@GetMapping("/greeting-i18n")
+	public String greeting(@RequestHeader(name = "Accept-Language", required = false) Locale locale) {
+		return messageSource.getMessage("good.morning.message", null, locale);
 	}
 }
