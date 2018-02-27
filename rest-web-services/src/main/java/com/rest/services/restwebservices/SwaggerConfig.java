0e78df6da21e9785ce054c14b8e04875c624a90f
Arvind <europa1613@users.noreply.github.com>
2018-02-26 22:30:00 -0600

    Produces and Consumes

--- rest-web-services/src/main/java/com/rest/services/restwebservices/SwaggerConfig.java
+++ rest-web-services/src/main/java/com/rest/services/restwebservices/SwaggerConfig.java
@@ -1,5 +1,9 @@
 package com.rest.services.restwebservices;
 
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -16,9 +20,12 @@ public class SwaggerConfig {
 	public static final Contact DEFAULT_CONTACT = new Contact("Andromeda", "www.andromeda.com", "111-111-1111");
 	public static final ApiInfo API_INFO = new ApiInfo("Restful Api", "Restful Api Desc", "1.0", "urn:tos",
 			DEFAULT_CONTACT, "Apache 2.0", "http://www.apache.org/licenses/LICENSE-2.0");
+	private static final Set<String> PRODUCES_AND_CONSUMES = new HashSet<>(
+			Arrays.asList("application/json", "application/xml"));
 
 	@Bean
 	public Docket api() {
-		return new Docket(DocumentationType.SWAGGER_2).apiInfo(API_INFO);
+		return new Docket(DocumentationType.SWAGGER_2).apiInfo(API_INFO).produces(PRODUCES_AND_CONSUMES)
+				.consumes(PRODUCES_AND_CONSUMES);
 	}
 }
