938ccccf534e2c141837e6c812c25aab936590cc
Arvind <europa1613@users.noreply.github.com>
2018-03-18 19:44:38 -0500

    Versioning - Request Param

--- rest-web-services/src/main/java/com/rest/services/restwebservices/versioning/PersonVersioningController.java
+++ rest-web-services/src/main/java/com/rest/services/restwebservices/versioning/PersonVersioningController.java
@@ -15,4 +15,16 @@ public class PersonVersioningController {
 	public PersonV2 personV2() {
 		return new PersonV2(new Name("Bob", "Charlie"));
 	}
+
+	// /person/param?verion=1
+	@GetMapping(value = "/person/param", params = "version=1")
+	public PersonV1 paramV1() {
+		return new PersonV1("Bob Charlie");
+	}
+
+	// /person/param?verion=2
+	@GetMapping(value = "/person/param", params = "version=2")
+	public PersonV2 paramV2() {
+		return new PersonV2(new Name("Bob", "Charlie"));
+	}
 }
