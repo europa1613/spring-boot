301d1ede9d8c257c496e6fd56fed7c0a57aea0d4
Arvind <europa1613@users.noreply.github.com>
2018-03-18 19:38:33 -0500

    Versioning - Baisic approach with URIs

---
+++ rest-web-services/src/main/java/com/rest/services/restwebservices/versioning/Name.java
@@ -0,0 +1,34 @@
+package com.rest.services.restwebservices.versioning;
+
+public class Name {
+	private String firstname;
+
+	private String lastname;
+
+	public Name() {
+		super();
+	}
+
+	public Name(String firstname, String lastname) {
+		super();
+		this.firstname = firstname;
+		this.lastname = lastname;
+	}
+
+	public String getFirstname() {
+		return firstname;
+	}
+
+	public void setFirstname(String firstname) {
+		this.firstname = firstname;
+	}
+
+	public String getLastname() {
+		return lastname;
+	}
+
+	public void setLastname(String lastname) {
+		this.lastname = lastname;
+	}
+
+}
---
+++ rest-web-services/src/main/java/com/rest/services/restwebservices/versioning/PersonV2.java
@@ -0,0 +1,25 @@
+package com.rest.services.restwebservices.versioning;
+
+public class PersonV2 {
+
+	private Name name;
+
+	public PersonV2() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public PersonV2(Name name) {
+		super();
+		this.name = name;
+	}
+
+	public Name getName() {
+		return name;
+	}
+
+	public void setName(Name name) {
+		this.name = name;
+	}
+
+}
