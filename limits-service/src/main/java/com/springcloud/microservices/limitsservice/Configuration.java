4e97f0cecc8c82c0fad734654d8c5bf17213f078
Arvind <europa1613@users.noreply.github.com>
2018-03-24 17:34:44 -0500

    ConfigurationProperties component

---
+++ limits-service/src/main/java/com/springcloud/microservices/limitsservice/Configuration.java
@@ -0,0 +1,30 @@
+package com.springcloud.microservices.limitsservice;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Component
+@ConfigurationProperties("limits-service")
+public class Configuration {
+
+	private int maximum;
+
+	private int minimum;
+
+	public int getMaximum() {
+		return maximum;
+	}
+
+	public int getMinimum() {
+		return minimum;
+	}
+
+	public void setMaximum(int maximum) {
+		this.maximum = maximum;
+	}
+
+	public void setMinimum(int minimum) {
+		this.minimum = minimum;
+	}
+
+}
--- limits-service/src/main/java/com/springcloud/microservices/limitsservice/LimitsConfigurationController.java
+++ limits-service/src/main/java/com/springcloud/microservices/limitsservice/LimitsConfigurationController.java
@@ -1,5 +1,6 @@
 package com.springcloud.microservices.limitsservice;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,8 +9,11 @@ import com.springcloud.microservices.limitsservice.bean.LimitConfiguration;
 @RestController
 public class LimitsConfigurationController {
 
+	@Autowired
+	private Configuration config;
+
 	@GetMapping("/limits")
 	public LimitConfiguration getLimitsFromConfigurations() {
-		return new LimitConfiguration(1000, 1);
+		return new LimitConfiguration(config.getMaximum(), config.getMinimum());
 	}
 }
