c062cc59346afb6d9c14424c08862b3e7267c154
Arvind <europa1613@users.noreply.github.com>
2018-04-08 22:15:26 -0500

    Dynamic port - indicate to differentiate from which instance returns

--- currency-exchange-service/src/main/java/com/springcloud/microservices/currencyexchangeservice/CurrencyExchangeController.java
+++ currency-exchange-service/src/main/java/com/springcloud/microservices/currencyexchangeservice/CurrencyExchangeController.java
@@ -2,6 +2,8 @@ package com.springcloud.microservices.currencyexchangeservice;
 
 import java.math.BigDecimal;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,10 +11,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class CurrencyExchangeController {
 
+	@Autowired
+	Environment env;
+
 	@GetMapping("/currency-exchange/from/{from}/to/{to}")
 	public ExchangeValue getExchangeValue(@PathVariable String from, @PathVariable String to) {
 
-		return new ExchangeValue(1L, from, to, BigDecimal.valueOf(65));
+		ExchangeValue exchangeValue = new ExchangeValue(1L, from, to, BigDecimal.valueOf(65));
+		exchangeValue.setPort(Integer.parseInt(env.getProperty("local.server.port")));
+
+		return exchangeValue;
 	}
 
 }
