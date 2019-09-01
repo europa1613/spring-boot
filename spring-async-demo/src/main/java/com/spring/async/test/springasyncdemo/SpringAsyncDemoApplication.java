9299eb70e071920cc18dfac75116e295a0e01889
Arvind <europa1613@users.noreply.github.com>
2019-09-01 13:55:21 -0500

    Initial generated project

---
+++ spring-async-demo/src/main/java/com/spring/async/test/springasyncdemo/SpringAsyncDemoApplication.java
@@ -0,0 +1,13 @@
+package com.spring.async.test.springasyncdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAsyncDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAsyncDemoApplication.class, args);
+	}
+
+}
