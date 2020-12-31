4257c521bc9b40b6157fe7d3212825c9145479c3
Arvind <europa1613@users.noreply.github.com>
2020-12-31 16:22:52 -0600

    Initial - start.spring.io

---
+++ spring-reactive-demo/src/main/java/com/europa/spring/reactive/demo/SpringReactiveDemoApplication.java
@@ -0,0 +1,13 @@
+package com.europa.spring.reactive.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringReactiveDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringReactiveDemoApplication.class, args);
+	}
+
+}
---
+++ spring-reactive-demo/src/test/java/com/europa/spring/reactive/demo/SpringReactiveDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.europa.spring.reactive.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringReactiveDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
