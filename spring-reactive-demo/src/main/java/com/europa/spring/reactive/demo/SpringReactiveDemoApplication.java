759b9342cf0e5fdc447cd84b753ae3b1e4b12569
Arvind <europa1613@users.noreply.github.com>
2020-12-31 19:09:41 -0600

    Controller and event stream exposing

--- spring-reactive-demo/src/main/java/com/europa/spring/reactive/demo/SpringReactiveDemoApplication.java
+++ spring-reactive-demo/src/main/java/com/europa/spring/reactive/demo/SpringReactiveDemoApplication.java
@@ -12,8 +12,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.data.repository.reactive.ReactiveCrudRepository;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
@@ -22,6 +27,33 @@ public class SpringReactiveDemoApplication {
 
   public static void main(String[] args) {
     SpringApplication.run(SpringReactiveDemoApplication.class, args);
+  }
+
+}
+
+@RestController
+@RequestMapping("/coffees")
+class CoffeeController {
+
+  private final CoffeeService service;
+
+  CoffeeController(CoffeeService service) {
+    this.service = service;
+  }
+
+  @GetMapping
+  public Flux<Coffee> all() {
+    return service.getAllCoffees();
+  }
+
+  @GetMapping("/{id}")
+  public Mono<Coffee> byId(@PathVariable String id) {
+    return service.getCoffeeById(id);
+  }
+
+  @GetMapping(value = "/{id}/orders", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+  public Flux<CoffeeOrder> orders(@PathVariable String id) {
+    return service.getOrders(id);
   }
 
 }
