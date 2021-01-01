ac418e180feaf92130c90ccf84b8dd8d78b71a5d
Arvind <europa1613@users.noreply.github.com>
2020-12-31 23:39:14 -0600

    Collect all monos and Mono.when

--- spring-reactive-demo/src/main/java/com/europa/spring/reactive/demo/jokes/JokesReactiveWebClient.java
+++ spring-reactive-demo/src/main/java/com/europa/spring/reactive/demo/jokes/JokesReactiveWebClient.java
@@ -1,16 +1,14 @@
 package com.europa.spring.reactive.demo.jokes;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import java.time.Duration;
 import java.time.Instant;
-import java.util.Arrays;
-import java.util.Objects;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.web.client.RestTemplate;
 import org.springframework.web.reactive.function.client.WebClient;
-import org.springframework.web.util.DefaultUriBuilderFactory;
 import reactor.core.publisher.Mono;
 
 public class JokesReactiveWebClient {
@@ -25,13 +23,20 @@ public class JokesReactiveWebClient {
   public static void main(String[] args) throws JsonProcessingException {
 
     Instant start = Instant.now();
-    for (int i = 1; i < 4; i++) {
-      /*Arrays.stream(Objects.requireNonNull(client.get().uri("{count}/jokes?delay=2", 1).retrieve()
-          .bodyToMono(Joke[].class).block())).forEach(j -> logger.info("====> Jokes: {}", j));*/
+
+    List<Mono<Joke[]>> monoList = Stream.of(1, 2, 3)
+        .map(i -> client.get().uri("{count}/jokes?delay=2", 1).retrieve().bodyToMono(Joke[].class))
+        .collect(
+            Collectors.toList());
+    Mono.when(monoList).block();
+
+    /*for (int i = 1; i < 4; i++) {
+     *//*Arrays.stream(Objects.requireNonNull(client.get().uri("{count}/jokes?delay=2", 1).retrieve()
+          .bodyToMono(Joke[].class).block())).forEach(j -> logger.info("====> Jokes: {}", j));*//*
       Mono<Joke[]> jokesMono = client.get().uri("{count}/jokes?delay=2", 1).retrieve()
           .bodyToMono(Joke[].class);
-      jokesMono.subscribe(System.out::println);
-    }
+      //jokesMono.subscribe(System.out::println); doesn't work
+    }*/
     logger.info("====> Elapsed time: {}ms", Duration.between(start, Instant.now()).toMillis());
   }
 
