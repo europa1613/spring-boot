5a0ca41139b292bc25f44c08484a98c5bf9d6779
Arvind <europa1613@users.noreply.github.com>
2020-12-31 18:41:57 -0600

    Coffee service with coffee orders, sink and reactive test

--- spring-reactive-demo/src/main/java/com/europa/spring/reactive/demo/SpringReactiveDemoApplication.java
+++ spring-reactive-demo/src/main/java/com/europa/spring/reactive/demo/SpringReactiveDemoApplication.java
@@ -1,5 +1,6 @@
 package com.europa.spring.reactive.demo;
 
+import java.time.Duration;
 import java.time.Instant;
 import java.util.UUID;
 import javax.annotation.PostConstruct;
@@ -12,7 +13,9 @@ import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.data.repository.reactive.ReactiveCrudRepository;
 import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 @SpringBootApplication
 public class SpringReactiveDemoApplication {
@@ -23,6 +26,30 @@ public class SpringReactiveDemoApplication {
 
 }
 
+@Service
+class CoffeeService {
+
+  private final CoffeeRepository repo;
+
+  public CoffeeService(CoffeeRepository repo) {
+    this.repo = repo;
+  }
+
+  Flux<Coffee> getAllCoffees() {
+    return repo.findAll();
+  }
+
+  Mono<Coffee> getCoffeeById(String id) {
+    return repo.findById(id);
+  }
+
+  Flux<CoffeeOrder> getOrders(String coffeeId) {
+    return Flux.<CoffeeOrder>generate(sink -> sink.next(new CoffeeOrder(coffeeId, Instant.now())))
+        .delayElements(
+            Duration.ofSeconds(1));
+  }
+}
+
 @Component
 class DataLoader {
 
@@ -31,6 +58,7 @@ class DataLoader {
   public DataLoader(CoffeeRepository repo) {
     this.repo = repo;
   }
+
 
   @PostConstruct
   private void loadData() {
--- spring-reactive-demo/src/test/java/com/europa/spring/reactive/demo/SpringReactiveDemoApplicationTests.java
+++ spring-reactive-demo/src/test/java/com/europa/spring/reactive/demo/SpringReactiveDemoApplicationTests.java
@@ -1,13 +1,30 @@
 package com.europa.spring.reactive.demo;
 
+import java.time.Duration;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import reactor.test.StepVerifier;
 
 @SpringBootTest
 class SpringReactiveDemoApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+  @Autowired
+  private CoffeeService service;
+
+  @Test
+  void contextLoads() {
+  }
+
+  @Test
+  void getCoffeeOrders() {
+    String coffeeId = service.getAllCoffees().blockFirst().getId();
+
+    StepVerifier.withVirtualTime(() -> service.getOrders(coffeeId).take(10))
+        .thenAwait(Duration.ofHours(10))
+        .expectNextCount(10)
+        .expectComplete()
+        .verify();
+  }
 
 }
