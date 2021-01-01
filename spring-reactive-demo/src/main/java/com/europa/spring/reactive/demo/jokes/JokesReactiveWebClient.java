c7c42b2520aa394bb9da2d92069b2c4924bc4eff
Arvind <europa1613@users.noreply.github.com>
2020-12-31 23:45:35 -0600

    Using Flux

--- spring-reactive-demo/src/main/java/com/europa/spring/reactive/demo/jokes/JokesReactiveWebClient.java
+++ spring-reactive-demo/src/main/java/com/europa/spring/reactive/demo/jokes/JokesReactiveWebClient.java
@@ -1,15 +1,11 @@
 package com.europa.spring.reactive.demo.jokes;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import java.time.Duration;
 import java.time.Instant;
-import java.util.List;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.reactive.function.client.WebClient;
-import reactor.core.publisher.Mono;
+import reactor.core.publisher.Flux;
 
 public class JokesReactiveWebClient {
 
@@ -20,15 +16,21 @@ public class JokesReactiveWebClient {
   private static final WebClient client = WebClient.create(JOKES_API_URL);
 
 
-  public static void main(String[] args) throws JsonProcessingException {
+  public static void main(String[] args) {
 
     Instant start = Instant.now();
 
-    List<Mono<Joke[]>> monoList = Stream.of(1, 2, 3)
+    Flux.range(1, 3)
+        .flatMap(i -> client.get().uri("{count}/jokes?delay=2", 1)
+            .retrieve()
+            .bodyToMono(Joke[].class))
+        .blockLast();
+
+   /* List<Mono<Joke[]>> monoList = Stream.of(1, 2, 3)
         .map(i -> client.get().uri("{count}/jokes?delay=2", 1).retrieve().bodyToMono(Joke[].class))
         .collect(
             Collectors.toList());
-    Mono.when(monoList).block();
+    Mono.when(monoList).block();*/
 
     /*for (int i = 1; i < 4; i++) {
      *//*Arrays.stream(Objects.requireNonNull(client.get().uri("{count}/jokes?delay=2", 1).retrieve()
