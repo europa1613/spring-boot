27f6d397819e0e4ba1b4eabebe79b5963acec5b6
Arvind <europa1613@users.noreply.github.com>
2021-01-09 13:32:56 -0600

    Initial commit - springio generated, Reactive Cassandra unit

---
+++ spring-reactive-cassandra-unit/src/main/java/com/europa/spring/reactive/cassandra/unit/SpringReactiveCassandraUnitApplication.java
@@ -0,0 +1,13 @@
+package com.europa.spring.reactive.cassandra.unit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringReactiveCassandraUnitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringReactiveCassandraUnitApplication.class, args);
+	}
+
+}
---
+++ spring-reactive-cassandra-unit/src/test/java/com/europa/spring/reactive/cassandra/unit/SpringReactiveCassandraUnitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.europa.spring.reactive.cassandra.unit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringReactiveCassandraUnitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
