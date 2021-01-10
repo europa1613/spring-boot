51d7b83777a38515ae46fadf51819fa8b5aacc05
Arvind <europa1613@users.noreply.github.com>
2021-01-09 20:41:03 -0600

    intial commit - springio raw

---
+++ springboot-https-demo/src/main/java/com/europa/springboot/https/demo/SpringbootHttpsDemoApplication.java
@@ -0,0 +1,13 @@
+package com.europa.springboot.https.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootHttpsDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootHttpsDemoApplication.class, args);
+	}
+
+}
---
+++ springboot-https-demo/src/test/java/com/europa/springboot/https/demo/SpringbootHttpsDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.europa.springboot.https.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootHttpsDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
