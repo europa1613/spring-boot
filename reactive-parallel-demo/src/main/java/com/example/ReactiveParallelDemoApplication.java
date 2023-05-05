f1a283ed679141aa42255f9502b1587531134aac
Arvind <europa1613@users.noreply.github.com>
2023-05-04 20:25:24 -0500

    initial - reactive parallel

---
+++ reactive-parallel-demo/src/main/java/com/example/ReactiveParallelDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactiveParallelDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactiveParallelDemoApplication.class, args);
+	}
+
+}
